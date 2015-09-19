6614db05e2b2e3e36fce880261b1c64a986d6b18
boxeelab <pingdjm@gmail.com>
2015-09-18 23:32:50 -0400

    Simple thread application demo

--- SimpleThread/app/src/main/java/com/boxeelab/playground/simplethread/simplethread/MainActivity.java
+++ SimpleThread/app/src/main/java/com/boxeelab/playground/simplethread/simplethread/MainActivity.java
@@ -2,10 +2,20 @@ package com.boxeelab.playground.simplethread.simplethread;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+
+/**
+ * This application demonstrate simple java thread.
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String APP_DEBUG = "SIMPLE_THREAD_DEBUG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +43,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This method is called using android:onClick from activity_main.xml
+     * @param view
+     */
+    public void onClick(View view)
+    {
+        runSimpleThread();
+    }
+
+    /**
+     *  Thread does not do anything. runs a for to simulate a long task
+     */
+    public void runSimpleThread()
+    {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // running simple loop here  with a 100 millisecond delay
+                for(int i =0 ;i<1000;i++)
+                {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d(APP_DEBUG,"Running simple thread");
+                }
+
+            }
+        });
+        thread.start();
+
     }
 }

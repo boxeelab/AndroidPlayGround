d2d92299fd5eb008bfb73221d66cf94253da320e
boxeelab <pingdjm@gmail.com>
2015-09-18 23:55:15 -0400

    Async Thread application added

---
+++ AsyncTaskDemo/app/src/main/java/com/boxeelab/playground/asynctaskdemo/asynctaskdemo/MainActivity.java
@@ -0,0 +1,92 @@
+package com.boxeelab.playground.asynctaskdemo.asynctaskdemo;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This method is called using android:onClick from activity_main.xml
+     * @param view
+     */
+    public void onClick(View view)
+    {
+        runAsyncThread();
+    }
+
+    private void runAsyncThread()
+    {
+        AsyncThreadDemo asyncThreadDemo = new AsyncThreadDemo();
+        asyncThreadDemo.doInBackground(100);
+    }
+
+    /**
+     * Async thread demo class
+     */
+
+    class AsyncThreadDemo extends AsyncTask<Integer,Integer,Integer>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled(Integer integer) {
+            super.onCancelled(integer);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            return null;
+        }
+    }
+
+}
